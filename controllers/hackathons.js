f0a2426433b5c41e0bee427c8ac20cbeb3c8f682
Parinith Shekar <parinith.1114@gmail.com>
2019-03-24 08:09:48 +0530

    Hacks page done

--- controllers/hackathons.js
+++ controllers/hackathons.js
@@ -13,3 +13,28 @@ exports.index = (req, res) => {
 		});})
 	
 };
+
+exports.hacks = (req, res) => {
+	// console.log(req);
+	axios.get(`https://mlh-events.now.sh/na-2019`)
+	// .then(resp => resp.json())
+	.then( response => {
+		// console.log(response);
+		const allHacks = response.data;
+		console.log(response.data[0]);
+		
+		allHacks.forEach( hack => {
+			if(hack.name==req.query.hack) {
+				res.render('hacks', hack)
+			}
+		})
+		
+		// res.render('hacks', {
+		// 	hack: response.data[0].name
+		// })
+	})
+	.catch( err => {
+		console.log(err);
+	});
+	
+}
